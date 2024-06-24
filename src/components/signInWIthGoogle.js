8163fc3991866eb359810123770a8081c43d3eac
CHANDIKASREEAKHIL <chandikasreeakhil@gmail.com>
2024-06-24 23:19:04 +0530

    Update signInWIthGoogle.js

--- src/components/signInWIthGoogle.js
+++ src/components/signInWIthGoogle.js
@@ -6,14 +6,21 @@ import { setDoc, doc } from "firebase/firestore";
 function SignInwithGoogle() {
   function googleLogin() {
     const provider = new GoogleAuthProvider();
-    provider.then(async (result) => {
+    signInWithPopup(auth, provider).then(async (result) => {
+      console.log(result);
       const user = result.user;
+      if (result.user) {
         await setDoc(doc(db, "Users", user.uid), {
           email: user.email,
           firstName: user.displayName,
           photo: user.photoURL,
           lastName: "",
         });
+        toast.success("User logged in Successfully", {
+          position: "top-center",
+        });
+        window.location.href = "/";
+      }
     });
   }
   return (
